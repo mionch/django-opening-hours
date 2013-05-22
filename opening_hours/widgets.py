79a4aca9c49c10b31de98452f177f84a16414997
Ondrej Sika <sika.ondrej@gmail.com>
2013-05-22 16:06:47 +0200

    fix template path

--- opening_hours/widgets.py
+++ opening_hours/widgets.py
@@ -7,7 +7,7 @@ from django.conf import settings
 class OpeningHoursWidget(Widget):
     is_required = False
     def render(self, name, value, attrs=None):
-        return mark_safe(loader.render_to_string("opening_hours_field/widget.html", {
+        return mark_safe(loader.render_to_string("opening_hours/widget.html", {
             "value": value,
             "STATIC_URL": settings.STATIC_URL,
         }))
