ec16b9e3fff1db3f66244d61c406a4c047b80ae6
Ondrej Sika <sika.ondrej@gmail.com>
2013-05-22 23:10:51 +0200

    fix blank list

--- opening_hours/widgets.py
+++ opening_hours/widgets.py
@@ -10,7 +10,7 @@ class OpeningHoursWidget(Widget):
     is_required = False
     def render(self, name, value, attrs=None):
         if not value:
-            value = '{"mo":[["", ""]], "tu":[["", ""]], "we":[["", ""]], "th":[["", ""]], "fr":[["", ""]], "sa":[["", ""]], "su":[["", ""]], }'
+            value = '{"mo":[["", ""]], "tu":[["", ""]], "we":[["", ""]], "th":[["", ""]], "fr":[["", ""]], "sa":[["", ""]], "su":[["", ""]]}'
         return mark_safe(loader.render_to_string("opening_hours/widget.html", {
             "value": value,
             "name": name,
