14d656a5b3e01279910ad818cc9c038c8adea096
Ondrej Sika <sika.ondrej@gmail.com>
2013-05-31 16:24:12 +0200

    add south migration support, v.1.2.2

--- opening_hours/db_fields.py
+++ opening_hours/db_fields.py
@@ -14,3 +14,9 @@ class OpeningHoursField(models.Field):
         }
         defaults.update(kwargs)
         return super(OpeningHoursField, self).formfield(**defaults)
+
+    def south_field_triple(self):
+        from south.modelsinspector import introspector
+        field_class = "django.db.models.fields.TextField"
+        args, kwargs = introspector(self)
+        return (field_class, args, kwargs)
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-opening-hours",
-    version = "1.2.1",
+    version = "1.2.2",
     url = 'http://ondrejsika.com/docs/django-opening-hours/',
     download_url = 'https://github.com/sikaondrej/django-opening-hours/',
     license = 'GNU LGPL v.3',
