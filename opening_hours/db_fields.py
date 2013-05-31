0cb687d187444fb53711cd52baa8c8d68d07a08a
Ondrej Sika <sika.ondrej@gmail.com>
2013-05-31 15:57:54 +0200

    add db type to model, v.1.2.1

--- opening_hours/db_fields.py
+++ opening_hours/db_fields.py
@@ -4,6 +4,9 @@ import form_fields
 import widgets
 
 class OpeningHoursField(models.Field):
+    def get_internal_type(self):
+        return "TextField"
+
     def formfield(self, **kwargs):
         defaults = {
             'form_class': form_fields.OpeningHoursField,
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "django-opening-hours",
-    version = "1.2.0",
+    version = "1.2.1",
     url = 'http://ondrejsika.com/docs/django-opening-hours/',
     download_url = 'https://github.com/sikaondrej/django-opening-hours/',
     license = 'GNU LGPL v.3',
