e2902a2a7056f349338aad767347524e43740b7b
Ondrej Sika <sika.ondrej@gmail.com>
2013-07-30 10:22:52 +0200

    add util is_open, v1.3.0

---
+++ opening_hours/utils.py
@@ -0,0 +1,15 @@
+def is_open(date, opening_hours):
+    try:
+        day = opening_hours[date.strftime("%a")[:2].lower()]
+    except KeyError:
+        return False
+    for opening in day:
+        start = opening[0].split(":")
+        end = opening[1].split(":")
+        start = int(start[0])*60 + int(start[1])
+        end = int(end[0])*60 + int(end[1])
+        now = date.hour*60 + date.minute
+        if start < now < end:
+            return True
+    return False
+
